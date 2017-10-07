96794634ce4b63b599558aca1879dd75c7b742b6
Rene van Putten <z669016@gmail.com>
2017-10-07 18:56:38 +0200

    Added return codes from the main (0 = OK, 1 = HELP, 2 = ERROR)

--- src/main/java/nl/putoet/bookmanager/AppMain.java
+++ src/main/java/nl/putoet/bookmanager/AppMain.java
@@ -5,11 +5,20 @@ import nl.putoet.ebooks.EBookTitleList;
 import java.nio.file.Paths;
 
 public class AppMain {
-    public static void main(String[] args) {
+    private static final int OK = 0;
+    private static final int HELP = 1;
+    private static final int ERROR = 2;
+
+    public static int main(String[] args) {
         System.out.println("[EBooks version 0.2]\n");
 
         final AppCommandLine commandLine = AppCommandLine.getInstance(args);
         final String commands[] = commandLine.getCommands();
+
+        if (commandLine.help()) {
+            commandLine.showHelp();
+            return HELP;
+        }
 
         if (commands.length > 0) {
             switch (commands[0].toUpperCase()) {
@@ -17,11 +26,13 @@ public class AppMain {
                     final App app = new App(commandLine);
                     final EBookTitleList list = app.list(Paths.get(commands[1]));
                     app.print(list);
-                    break;
+                    return OK;
 
                 default:
                     System.out.println("Unknown command '" + commands[0] + "'");
             }
         }
+
+        return ERROR;
     }
 }
