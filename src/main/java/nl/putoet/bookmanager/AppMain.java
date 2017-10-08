34e6fb754a896d2c8dfa0a226b1855041e77a6a1
Rene van Putten <z669016@gmail.com>
2017-10-08 06:59:53 +0200

    Return value through System.exit (return is not allowed)

--- src/main/java/nl/putoet/bookmanager/AppMain.java
+++ src/main/java/nl/putoet/bookmanager/AppMain.java
@@ -9,7 +9,7 @@ public class AppMain {
     private static final int HELP = 1;
     private static final int ERROR = 2;
 
-    public static int main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("[EBooks version 0.2]\n");
 
         final AppCommandLine commandLine = AppCommandLine.getInstance(args);
@@ -17,7 +17,7 @@ public class AppMain {
 
         if (commandLine.help()) {
             commandLine.showHelp();
-            return HELP;
+            System.exit(HELP);
         }
 
         if (commands.length > 0) {
@@ -26,13 +26,13 @@ public class AppMain {
                     final App app = new App(commandLine);
                     final EBookTitleList list = app.list(Paths.get(commands[1]));
                     app.print(list);
-                    return OK;
+                    System.exit(OK);
 
                 default:
                     System.out.println("Unknown command '" + commands[0] + "'");
             }
         }
 
-        return ERROR;
+        System.exit(ERROR);
     }
 }
